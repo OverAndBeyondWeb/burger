83f9baca367715065fe2dad68530e25a46ee2e94
Juan Fox <foxj0875@gmail.com>
2018-04-05 15:24:54 -0400

    burgers_db is set up with schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("bacon burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("veggie burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("blue cheese burger");
