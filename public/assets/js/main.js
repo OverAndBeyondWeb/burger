22e8599291da4d1166aaab7b553b8ab72c73eab8
Juan Fox <foxj0875@gmail.com>
2018-04-16 13:28:22 -0400

    jquery get request to '/api/all-burgers' is retreiving the data properly

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('hello');
+$.get('/api/all-burgers', function(data) {
+  console.log(data);
+  data.forEach(function(element) {
+    console.log(element);
+    var burger = $('<li>');
+    burger.text(element.burger_name);
+    var devourBtn = $('<button class="devour">Devour</button>');
+    burger.append(devourBtn);
+    $('.burger-list').append(burger);
+  })
+});
